760c81500b34083642a1b9e1a10bac9c0fb45c37
Артём Ганеев <a.ganeev@brandsgo.digital>
2025-08-20 10:27:01 +0300

    updated port

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ async def ws_endpoint(ws: WebSocket, user_id: str):
         connections.pop(user_id, None)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=433)
+    uvicorn.run("main:app", host="0.0.0.0", port=443)
